aa7a6c80c240f40fd81920498e13a6d297c6d95e
David Jacques <mail@davidprojects.xyz>
2024-12-30 13:34:30 -0500

    Refactor project structure: update working directory, switch to renv for dependency management, and modify baseURL in configuration. Restore PDF generation functionality in build script.

--- R/build.R
+++ R/build.R
@@ -1,2 +1,9 @@
 # An optional custom script to run before Hugo builds your site.
 # You can delete it if you do not need it.
+
+
+# Source helper functions
+source("R/generate_pdfs.R")
+
+# Generate PDFs for reports
+generate_report_pdfs("content/reports/tree-equity-score")
